7160551e6b4e19cbb53fe3ee52ad5f07d334593f
Nithin-Marla <marlanithin288@gmail.com>
2025-07-23 21:04:35 +0530

    add solution to find largest element in an array

---
+++ Geeks_For_Geeks/Largest_Element_in_array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static int largest(int[] arr) {
+        int n=arr[0];
+       for(int i=1;i<arr.length;i++){
+        if(arr[i]>n){
+            n=arr[i];
+        }
+       }
+     return n;    
+    }
+}
